c53f72180febf0618aa3b2aa284ca94a1f3b271f
Lu Vy <luvy@umich.edu>
2025-05-12 22:53:52 -0400

    Add files via upload

--- 1218-longest-arithmetic-subsequence-of-given-difference.py
+++ 1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference
 
+# brute force recursive DP -- DO NOT implement
+
 class Solution(object):
     def longestSubsequence(self, arr, difference):
         """
@@ -12,14 +14,16 @@ class Solution(object):
             if i == n-1:
                 return 1
             
-            k = min([j for j in range(i+1,n) if arr[j] == arr[i] + difference] + [float('inf')])
+            options = [j for j in range(i+1,n) if arr[j] == arr[i] + difference]
 
-            if k < float('inf'):
-                return 1 + v(k)
+            if options:
+                return 1 + v(min(options))
             else:
                 return 1
 
         return max(v(i) for i in range(n))
+    
+# tabulated DP with hash table
     
 class Solution(object):
     def longestSubsequence(self, arr, difference):
