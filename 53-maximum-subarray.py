e380bde42e9dd47aa7cbd2a4d462de727087caa0
Lu Vy <luvy@umich.edu>
2025-06-12 20:52:14 -0400

    Update 53-maximum-subarray.py

--- 53-maximum-subarray.py
+++ 53-maximum-subarray.py
@@ -1,11 +1,23 @@
 # https://leetcode.com/problems/maximum-subarray
 
+from typing import List
+from functools import cache
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        @cache
+        def v(i):
+            if i == n-1:
+                return nums[n-1]
+            
+            return nums[i] + max(v(i+1), 0)
+
+        return max(v(i) for i in range(n))
+
 class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         v = [None] * n
         v[n-1] = nums[n-1]
@@ -15,18 +27,14 @@ class Solution(object):
 
         return max(v)
         
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         v = nums[n-1]
-        ans = nums[n-1]
+        M = nums[n-1]
 
         for i in reversed(range(n-1)):
             v = nums[i] + max(v, 0)
-            ans = max(ans, v)
+            M = max(M, v)
 
-        return ans
+        return M
