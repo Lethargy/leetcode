bc1a0c55996fd2b925e27249b26c87dc06e0ecfe
Lu Vy <luvy@umich.edu>
2025-08-12 22:05:06 -0600

    Update 112-path-sum.py

--- 112-path-sum.py
+++ 112-path-sum.py
@@ -1,35 +1,45 @@
 # https://leetcode.com/problems/path-sum
 
+from collections import deque
+from typing import Optional
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+  
+# DFS
 
-# DFS iterative
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:           
+        def dfs(node,target):
+            if not node:
+                return False
+            if not (node.left or node.right): # leaf
+                return target == node.val
+            return dfs(node.left, target-node.val) or dfs(node.right, target-node.val)
 
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: Optional[TreeNode]
-        :type targetSum: int
-        :rtype: bool
-        """
-        if not root:
-            return False
+        return dfs(root,targetSum)
+    
+# BFS
 
-        stack = [(root, root.val)]
+from collections import deque
 
-        while stack:
-            node, runningSum = stack.pop()
-
-            if not (node.left or node.right) and runningSum == targetSum:
-                return True
-
-            if node.left:
-                stack.append((node.left, runningSum + node.left.val))
-
-            if node.right:
-                stack.append((node.right, runningSum + node.right.val))
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:           
+        
+        queue = deque([(root,targetSum)])
+        while queue:
+            node, target = queue.popleft()
+            if not node:
+                continue
+            if not (node.left or node.right):
+                if target == node.val:
+                    return True
+                else:
+                    continue
+            queue.append((node.left, target-node.val))
+            queue.append((node.right, target-node.val))
 
         return False
